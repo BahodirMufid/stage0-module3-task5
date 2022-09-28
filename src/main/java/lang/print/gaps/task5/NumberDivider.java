49752742f97124d5a0ffd566a2951692722f0e05
bahod <bahodirmufid@gmail.com>
2022-09-28 17:35:18 +0500

    all classes up to date

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+
+    public static void main(String[] args) {
+        divide(19);
+    }
+
+    static void divide(float toBeDivided) {
+        System.out.println((toBeDivided/5));
     }
 }
